fc56c6e8b4c9d40b3746d2887c1d3bdcaf707b65
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-04-18 17:38:22 -0400

    update with almost finished code

--- js/script.js
+++ js/script.js
@@ -31,59 +31,79 @@ var lowerArr = lower.split("");
 var upperArr = upper.split("");
 var specialArr = special.split("");
 
-console.log(num);
-console.log(numArr);
-console.log(lower);
-console.log(lowerArr);
-console.log(upper);
-console.log(upperArr);
-console.log(special);
-console.log(specialArr);
+// Create an empty array to concatenate array choice based on user input.
+var choiceArray = [];
+var randomPass = "";
 
-// This array concatenates all of the imput array choices.
-var pswdArray = numArr.concat(lowerArr, upperArr, specialArr);
-console.log(pswdArray);
+// // This array concatenates all of the imput array choices.
+// var pswdArray = numArr.concat(lowerArr, upperArr, specialArr);
+// Don't need this because added choiceArray
 
-  //Window prompt for user to choose password length; stored to variable
+
+  // Window prompt for user to choose password length; stored to variable
   var pwdLength = window.prompt("To generate a password: \n\nBegin by choosing a length between 8 and 128 characters.");
-  console.log(pwdLength);
     
-    //Alert for incorrect input and cancel function
+    // Alert for incorrect input and cancel function
     if (pwdLength < 8 || pwdLength > 128) {
       window.alert("\nYou did not input a valid length between 8 and 128 characters. \n\nYour password was not created. \n\nPlease try again."); 
-      console.log("invalid length");
       return;
       // Alert confirmation of input and continue password generation.
     } else if (pwdLength >= 8 || pwdLength <= 128) {
       window.alert("\nYou have entered a value of " + pwdLength + " for your password. \n\nClick 'OK' to continue.");
-      console.log("valid password length");
     }
 
   // Add confirmation box input for character types.
   var numbers = window.confirm("\nWould you like to include 'numbers' (0, 1, 2, etc.) in your password? \n\nIf yes, click 'OK'.");
-  console.log(numbers);
   var lwrCase = window.confirm("\nWould you like to include 'lowercase' characters in your password? \n\nIf yes, click 'OK'.");
-  console.log(lwrCase)
   var upCase = window.confirm("\nWould you like to include 'UPPERCASE' characters in your password? \n\nIf yes, click 'OK'.");
-  console.log(upCase);
   var spChar = window.confirm("\nWould you like to include 'special characters' (such as: !@#$%) in your password? \n\nIf yes, click 'OK'.");
-  console.log(spChar);
 
-  // alert if no character types were chosen and stop function
+    // Alert if no character types were chosen and stop function.
     if (numbers !== true && lwrCase !== true && upCase !== true && spChar !== true){
       window.alert("\nYou must choose at least one character type. \n\nYour password was not created.");
-      console.log("no character types");
       return;
     }
 
 
-  // action if any character type chosen
+    // Action if any character type chosen.
     if (numbers === true || lwrCase === true || upCase === true || spChar === true){
       window.confirm("\nClick 'OK' to generate your password.");
-      console.log("character types chosen")
+    }
+
+    // Push arrays together if variable chosen in confirm boxes.
+    if (numbers === true){
+      choiceArray.push(...numArr);
+    }
+    if (lwrCase === true){
+      choiceArray.push(...lowerArr);  
+    }
+    if (upCase === true){
+      choiceArray.push(...upperArr);
+    }
+    if (spChar === true){
+      choiceArray.push(...specialArr);
     }
 
 
+
+    var rando1 = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    console.log(rando1);
+
+
+
+
+    console.log(choiceArray);
+    console.log(choiceArray.toString());
+    console.log(choiceArray.join(""));
+
+    // Create random generated variable from the pushed arrays.
+    for (var i = 0; i < pwdLength; i++); 
+    
+
+
+
+    // var randomPswd = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    // console.log(randomPswd);
 
 }
 
